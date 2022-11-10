7f510d40aadf8ce361b710167254c834950543f1
Alexander Komarov <alexkom18@gmail.com>
2022-11-10 21:26:39 +0300

    Завершение рефакторинга routes

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-// const { celebrate, Joi } = require('celebrate');
-// const validator = require('validator');
 const { errors } = require('celebrate');
-
-// const auth = require('./middleware/auth');
-// const { login, createUsers } = require('./controllers/users');
-// const usersRouter = require('./routes/users');
-// const cardsRouter = require('./routes/cards');
 const cenralErrors = require('./middleware/centralError');
+const routers = require('./routes/index');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 
@@ -21,47 +15,8 @@ app.use(express.json());
 
 mongoose.connect(MONGO_URL, { autoIndex: true });
 
-const routers = require('./routes/index');
-
 app.use(routers);
 
-// app.post('/signin', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().required().custom((value, helpers) => {
-//       if (validator.isEmail(value)) { return value; }
-//       return helpers.message('Некорректный email');
-//     }),
-//     password: Joi.string().required(),
-//   }),
-// }), login);
-
-// app.post('/signup', (celebrate({
-//   body: Joi.object().keys({
-//     name: Joi.string().min(2).max(30),
-//     about: Joi.string().min(2).max(30),
-//     avatar: Joi.string().custom((value, helpers) => {
-//       if (/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/.test(value)) {
-//         return value;
-//       }
-//       return helpers.message('Некорректная ссылка');
-//     }),
-//     email: Joi.string().required().custom((value, helpers) => {
-//       if (validator.isEmail(value)) {
-//         return value;
-//       }
-//       return helpers.message('Некорректный email');
-//     }),
-//     password: Joi.string().required(),
-//   }),
-// })), createUsers);
-
-// app.use(auth);
-
-// app.use('/users', usersRouter);
-
-// app.use('/cards', cardsRouter);
-
-//
 app.use(errors());
 
 app.use(cenralErrors);
