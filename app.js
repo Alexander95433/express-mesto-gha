e9285f8fc4735ef7cf6b7470d53b05a9b7e1f928
Alexander Komarov <alexkom18@gmail.com>
2022-11-10 17:18:46 +0300

    -

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
+const auth = require('./middleware/auth');
+const { login, createUsers } = require('./controllers/users');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const ErrorNotFound = require('./errors/ErrorNotFound');
+
 const cenralErrors = require('./middleware/centralError');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 
 const app = express();
 
-const routers = require('./routes/index');
+// const routers = require('./routes/index');
 
 app.use(cookieParser());
 
@@ -16,8 +26,47 @@ app.use(express.json());
 
 mongoose.connect(MONGO_URL, { autoIndex: true });
 
-app.use(routers);
+// app.use(routers);
 
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().custom((value, helpers) => {
+      if (validator.isEmail(value)) { return value; }
+      return helpers.message('Некорректный email');
+    }),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', (celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom((value, helpers) => {
+      if (/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/.test(value)) {
+        return value;
+      }
+      return helpers.message('Некорректная ссылка');
+    }),
+    email: Joi.string().required().custom((value, helpers) => {
+      if (validator.isEmail(value)) {
+        return value;
+      }
+      return helpers.message('Некорректный email');
+    }),
+    password: Joi.string().required(),
+  }),
+})), createUsers);
+
+app.use(auth);
+
+app.use('/users', usersRouter);
+
+app.use('/cards', cardsRouter);
+
+app.use('*', (req, res, next) => { next(new ErrorNotFound('Запрашиваемый ресурс не найден')); });
+
+//
 app.use(errors());
 
 app.use(cenralErrors);
