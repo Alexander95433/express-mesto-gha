d816bfcda4e5db17f49644028cd49ac21388c03e
Alexander Komarov <alexkom18@gmail.com>
2022-11-10 17:08:06 +0300

    -

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 const BadAuthError = require('../errors/BadAuthError');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return new BadAuthError('Необходима авторизация');
-  }
-  const token = authorization.replace('Bearer ', '');
-  //const token = req.cookies.jwt;
+  // const { authorization } = req.headers;
+  // if (!authorization || !authorization.startsWith('Bearer ')) {
+  //   return new BadAuthError('Необходима авторизация');
+  // }
+  // const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
   try { payload = jwt.verify(token, 'some-secret-key'); } catch (err) {
     return new BadAuthError('Необходима авторизация');
