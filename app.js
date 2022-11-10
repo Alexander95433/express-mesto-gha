31a295b754e6c43a796a52a591bd50f2225be187
Alexander Komarov <alexkom18@gmail.com>
2022-11-10 17:31:24 +0300

    -

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 const { errors } = require('celebrate');
 
 // const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-const validator = require('validator');
 const auth = require('./middleware/auth');
 const { login, createUsers } = require('./controllers/users');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
-const ErrorNotFound = require('./errors/ErrorNotFound');
-
 const cenralErrors = require('./middleware/centralError');
+// const ErrorNotFound = require('./errors/ErrorNotFound');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 
 const app = express();
-
-// const routers = require('./routes/index');
 
 app.use(cookieParser());
 
 app.use(express.json());
 
 mongoose.connect(MONGO_URL, { autoIndex: true });
-
-// app.use(routers);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -64,7 +59,7 @@ app.use('/users', usersRouter);
 
 app.use('/cards', cardsRouter);
 
-app.use('*', (req, res, next) => { next(new ErrorNotFound('Запрашиваемый ресурс не найден')); });
+app.use('*', (req, res) => { res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }); });
 
 //
 app.use(errors());
