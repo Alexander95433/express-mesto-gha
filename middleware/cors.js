81b8f82a46d2b042393cc28c576b19cf4ab69c7f
Alexander Komarov <alexkom18@gmail.com>
2022-11-26 00:07:20 +0300

    fixed: CORS

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,9 +1,10 @@
 module.exports.corsConfiguration = {
-  origin: [
-    'https://alex-mesto.nomoredomains.icu',
-    'http://alex-mesto.nomoredomains.icu',
-    'http://localhost:3000',
-  ],
+  // origin: [
+  //   'https://alex-mesto.nomoredomains.icu',
+  //   'http://alex-mesto.nomoredomains.icu',
+  //   'http://localhost:3000',
+  // ],
+  origin: true,
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
